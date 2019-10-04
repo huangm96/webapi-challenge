907ee0b6a8fcc5c1514ebfdb550c9b3d9ce46555
Min-Huang <minh.huang96@gmail.com>
2019-10-04 09:19:48 -0700

    added middleware for projectRouter

--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -17,21 +17,34 @@ router.get("/", (req, res) => {
         .json({ error: "The projects information could not be retrieved." });
     });
 });
+router.get("/:id", validateProjectId, (req, res) => {
+  projectModel
+    .get(req.params.id)
+    .then(project => {
+        res.send({
+            message: `Project Id: ${req.params.id}`,
+            projectData: project
+        });
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ error: "The projects information could not be retrieved." });
+    });
+});
 
-router.post("/", (req, res) => {
-  if (!req.body.name || !req.body.description) {
-    res.status(400).json({ message: "missing name or description" });
-  } else {
+router.post("/", validateProject,(req, res) => {
+  
     projectModel
       .insert(req.body)
       .then(res.status(201).json(req.body))
       .catch({
         error: "There was an error while saving the project to the database"
       });
-  }
+  
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id",  validateProjectId,(req, res) => {
   projectModel
     .remove(req.params.id)
     .then(
@@ -44,7 +57,7 @@ router.delete("/:id", (req, res) => {
     });
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id",validateProject,  validateProjectId,(req, res) => {
   projectModel
     .update(req.params.id, req.body)
     .then(
@@ -58,7 +71,7 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.get('/:id/project-actions', (req, res) => {
+router.get('/:id/project-actions', validateProjectId, (req, res) => {
     projectModel
       .getProjectActions(req.params.id)
       .then(action => {
@@ -68,4 +81,34 @@ router.get('/:id/project-actions', (req, res) => {
         error: "The actions for this project could not be retrieved."
       });
 })
+
+// middleware
+
+function validateProject(req, res, next) {
+    if (!req.body) {
+        res.status(400).json({error:"missing project data"})
+    } else if (!req.body.name || !req.body.description) {
+         res.status(400).json({ error: "missing name or description for this project" });
+    } else {
+        next();
+    }
+}
+
+function validateProjectId(req, res, next) {
+   projectModel.get(req.params.id).then(project => {
+       if (!project) {
+           res.status(404).json({
+             error: "invalid project id"
+           });
+       } else {
+           next();
+        }
+   }).catch({
+       error: "The projects information could not be retrieved."
+    })
+}
+
+
+
+
 module.exports = router;
