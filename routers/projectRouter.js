45582656f43a2381fd99ca340e7b2578774f3e83
Min-Huang <minh.huang96@gmail.com>
2019-10-04 08:47:26 -0700

    finished project post request

--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -15,6 +15,17 @@ router.get("/", (req, res) => {
   );
 });
 
-
+router.post("/", (req, res) => {
+    if (!req.body.name || !req.body.description) {
+        res.status(400).json({message:"missing name or description"})
+    } else {
+        projectModel
+          .insert(req.body)
+          .then(res.status(201).json(req.body))
+          .catch({
+            error: "There was an error while saving the project to the database"
+          });
+    }
+})
 
 module.exports = router;
