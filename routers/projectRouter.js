31319ee0b105d8eb35098619c7d6e5ffa8192c45
Min-Huang <minh.huang96@gmail.com>
2019-10-04 09:04:39 -0700

    finished project put, delete, and getProjectAction request

--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -1,31 +1,71 @@
 const express = require("express");
 
 const projectModel = require("../data/helpers/projectModel.js");
+const actionModel = require('../routers/actionRouter.js')
+
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  projectModel.get().then(projects => {
-    res.send(projects);
-  }).catch(() => {
+  projectModel
+    .get()
+    .then(projects => {
+      res.send(projects);
+    })
+    .catch(() => {
       res
         .status(500)
         .json({ error: "The projects information could not be retrieved." });
-  }
-
-  );
+    });
 });
 
 router.post("/", (req, res) => {
-    if (!req.body.name || !req.body.description) {
-        res.status(400).json({message:"missing name or description"})
-    } else {
-        projectModel
-          .insert(req.body)
-          .then(res.status(201).json(req.body))
-          .catch({
-            error: "There was an error while saving the project to the database"
-          });
-    }
-})
+  if (!req.body.name || !req.body.description) {
+    res.status(400).json({ message: "missing name or description" });
+  } else {
+    projectModel
+      .insert(req.body)
+      .then(res.status(201).json(req.body))
+      .catch({
+        error: "There was an error while saving the project to the database"
+      });
+  }
+});
 
+router.delete("/:id", (req, res) => {
+  projectModel
+    .remove(req.params.id)
+    .then(
+      res.status(200).json({
+        message: `The project with id: ${req.params.id} was removed `
+      })
+    )
+    .catch({
+      error: "The project could not be removed"
+    });
+});
+
+router.put("/:id", (req, res) => {
+  projectModel
+    .update(req.params.id, req.body)
+    .then(
+      res.status(200).json({
+        message: `The project with id:${req.params.id} was updated`,
+        newData: req.body
+      })
+    )
+    .catch({
+      error: "The project information could not be modified."
+    });
+});
+
+router.get('/:id/project-actions', (req, res) => {
+    projectModel
+      .getProjectActions(req.params.id)
+      .then(action => {
+        res.send(action);
+      })
+      .catch({
+        error: "The actions for this project could not be retrieved."
+      });
+})
 module.exports = router;
